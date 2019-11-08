bd3292f7a01c10c79447c0fb43e7a0cbb0c23add
lihaolin <342161360@qq.com>
2019-11-09 00:22:45 +0800

    面向切面，集中式登录

---
+++ app/src/main/java/com/haolin/aop/sample/annotation/ClickBehavior.java
@@ -0,0 +1,17 @@
+package com.haolin.aop.sample.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 作者：haoLin_Lee on 2019/11/08 23:14
+ * 邮箱：Lhaolin0304@sina.com
+ * class:
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClickBehavior {
+    String value();
+}
